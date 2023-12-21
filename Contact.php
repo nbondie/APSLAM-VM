36d83f56902c6b1e4e2073a62193f20c58f2206c
nbondie <noe.bondiehouette@sts-sio-caen.info>
2023-12-21 11:14:25 +0100

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -43,7 +43,9 @@ $captcha="Fail" ;
 include_once 'yaml/vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-
+require "./PHPMailer/src/Exception.php";
+require "./PHPMailer/src/PHPMailer.php";
+require "./PHPMailer/src/SMTP.php";
 if(!empty($_POST)) {
  
     $mail = new PHPMailer(true);
@@ -60,19 +62,19 @@ if(!empty($_POST)) {
  
         //Recipients
         $mail->setFrom('noe.bondiehouette@sts-sio-caen.info', $_POST['from']);
-        $mail->addAddress($_POST['to']??'noe.bondiehouette@sts-sio-caen.info');     //Add a recipient
+        $mail->addAddress($_POST['to'], 'noe.bondiehouette@sts-sio-caen.info');     //Add a recipient
  
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
         $mail->Subject = $_POST['subject']??'Subject';
         $mail->Body = $_POST['body']??'This is the HTML message body <b>in bold!</b>';
+        
+        $res= "Le message a bien été envoyer";
         if ($captcha=="Succes"){
             $mail->send();
-            $res= "Le message a bien été envoyer";
         } else{
             $res= "Captchat non validé !";
-        }
-         catch (Exception $e) {
+    } catch (Exception $e) {
         $res= "Le message ne sait pas envoyer: {$mail->ErrorInfo} <br>Réessayer";
     }
 }
